c4fb9de86311e507e31f6f855a0e4bea03d70b2b
codehzl <45710922+codehzl@users.noreply.github.com>
2018-12-08 18:55:37 +0800

    Added initial files
    TESDA Wedding Theme Website

---
+++ script.js
@@ -0,0 +1,47 @@
+// ===== WOW Effects =====
+var wow = new WOW({
+  boxClass: 'wow', // animated element css class (default is wow)
+  animateClass: 'animated', // animation css class (default is animated)
+  offset: 0, // distance to the element when triggering the animation (default is 0)
+  mobile: true, // trigger animations on mobile devices (default is true)
+  live: true, // act on asynchronously loaded content (default is true)
+  callback: function(box) {
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+  scrollContainer: null, // optional scroll container selector, otherwise use window,
+  resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
+
+// ===== Scroll to Top ====
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// ===== Nav Bar Active Link =====
+
+// Active link
+
+
+// Hamburger menu toggle
+$(document).ready(function() {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
